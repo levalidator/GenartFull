55ab986fa5e1c49fd10d67ad5c6c838982879359
availlant <vaillant.alexis@hotmail.fr>
2013-05-24 23:02:20 +0200

    Signed-off-by: availlant <vaillant.alexis@hotmail.fr>

--- src/com/genart/DAL/DAOOrder.java
+++ src/com/genart/DAL/DAOOrder.java
@@ -1,17 +1,14 @@
 package com.genart.DAL;
 
 import java.sql.ResultSet;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import com.genart.bdd.AccesBDD;
-import com.genart.beans.Customer;
 import com.genart.beans.Order;
 import com.genart.beans.OrderLine;
-import com.sun.istack.internal.Nullable;
 
 public class DAOOrder {
 	public static List<Order> getListOrder(int idCustomer) {
